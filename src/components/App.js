0a092da733452df9b925538edbb607777600c79f
wangyiwei0108 <wangyiwei0108@gmail.com>
2021-06-01 14:26:22 +0800

    setup01

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import About from './About';
+import Contact from './Contact';
+import Footer from './Footer';
+import Navbar from './Navbar';
+import Work from './Work';
 
 const App = () => {
   return(
     <div>
-      Hey
+      <Navbar/>
+      <About/>
+      <Work/>
+      <Contact/>
+      <Footer/>
     </div>
   )
 }
